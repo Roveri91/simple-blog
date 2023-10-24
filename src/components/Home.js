3606a4a18bff3b095481de33275f8d1bf671a5e2
Simone Roveri <spikeroveri91@gmail.com>
2023-10-24 19:40:48 +0900

    creatre

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,20 @@
 import { useState } from "react";
 
 const Home = () => {
-
-  const [name, setName] = useState("Mario");
-  const [age, setAge] = useState(25);
-
-  const ButtonClick = () => {
-    setName('Luigi');
-    setAge(30);
-  }
-
+  const [blogs, setBlogs] = useState([
+    { title: "My first Blog", body: "lorem ipsum...", author: "Simone", id: 1},
+    {title: "Coding is Great", body: "lorem ipsum etc...", author: "Mark Zuckerberg", id: 2},
+    {title: "React is fun", body: "Even more lorem ipsum ...", author: "Elon Musk", id: 3}
+  ]);
 
   return (
     <div className="home">
-      <h2>homepage</h2>
-      <p>{ name } is  { age } years old</p>
-      <button onClick={ButtonClick}>Click Me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+        </div>
+      ))}
     </div>
   );
 }
