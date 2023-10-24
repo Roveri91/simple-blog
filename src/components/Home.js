20c18a0c2dd4e9c4cd04e51f8ff3f9dbbad0d93a
Simone Roveri <spikeroveri91@gmail.com>
2023-10-24 18:58:14 +0900

    playing with useState

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 const Home = () => {
+
+  const [name, setName] = useState("mario");
+
+  const ButtonClick = () => {
+    setName('Luigi')
+  }
+
+
   return (
     <div className="home">
       <h2>homepage</h2>
+      <p>{ name }</p>
+      <button onClick={ButtonClick}>Click Me</button>
     </div>
   );
 }
