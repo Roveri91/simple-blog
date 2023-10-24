53add0408c31d68855b98fdd0be18475254791d0
Simone Roveri <spikeroveri91@gmail.com>
2023-10-24 19:01:04 +0900

    change p when click

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,17 +2,19 @@ import { useState } from "react";
 
 const Home = () => {
 
-  const [name, setName] = useState("mario");
+  const [name, setName] = useState("Mario");
+  const [age, setAge] = useState(25);
 
   const ButtonClick = () => {
-    setName('Luigi')
+    setName('Luigi');
+    setAge(30);
   }
 
 
   return (
     <div className="home">
       <h2>homepage</h2>
-      <p>{ name }</p>
+      <p>{ name } is  { age } years old</p>
       <button onClick={ButtonClick}>Click Me</button>
     </div>
   );
