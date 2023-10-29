784c347b439224f6dacb9f8e149e3341d51fa2b2
Simone Roveri <spikeroveri91@gmail.com>
2023-10-29 10:23:34 +0900

    fetch data from db

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,24 +2,21 @@ import { useState,useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My first Blog", body: "lorem ipsum...", author: "Simone", id: 1},
-    {title: "Coding is Great", body: "lorem ipsum etc...", author: "Mark Zuckerberg", id: 2},
-    {title: "React is fun", body: "Even more lorem ipsum ...", author: "Elon Musk", id: 3}
-  ]);
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
+  const [blogs, setBlogs] = useState(null);
 
   useEffect(() => {
-    console.log('use effect ran');
+    fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+         setBlogs(data);
+      });
   }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} handleDelete={handleDelete}/>
+      {blogs && <BlogList blogs={blogs}/>}
     </div>
   );
 }
