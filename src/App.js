cda458283fcf3a74978c3646181dbaec475c37b4
Simone Roveri <spikeroveri91@gmail.com>
2023-11-02 03:00:04 +0900

    add routes to react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  const link = "http://www.google.com";
-
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
-     </div>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+      </div>
+      </div>
+    </Router>
   );
 }
 
