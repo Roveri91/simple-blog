661d90c8ff54cb50c331df17c724165e45bb9798
Simone Roveri <spikeroveri91@gmail.com>
2023-11-02 07:16:24 +0900

    fetch data for single blog

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,12 +1,24 @@
 import { useParams } from "react-router-dom";
+import Loading from "./Loading";
+import useFetch from "../useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
 
   return (
-    <div className="blog-details">
-      <h2>Blog details - { id }</h2>
-
+    <div>
+      { isPending && <div><Loading /></div> }
+      { error && <div>{ error }</div>}
+      { blog && (
+        <div className="blog-details">
+          <article>
+            <h2>{ blog.title }</h2>
+            <p><i>Written By:</i> { blog.author }</p>
+            <div>{ blog.body }</div>
+          </article>
+        </div>
+      )}
     </div>
   );
 }
