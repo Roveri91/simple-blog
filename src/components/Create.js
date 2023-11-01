41582075e08306c486783ea30ac47b045d9b3c50
Simone Roveri <spikeroveri91@gmail.com>
2023-11-02 08:31:28 +0900

    create a form for create

--- src/components/Create.js
+++ src/components/Create.js
@@ -2,6 +2,28 @@ const Create = () => {
   return (
     <div className="create">
       <h2>Add a new blog</h2>
+      <form>
+        <label>Tilte:</label>
+        <input
+          type="text"
+          required
+          placeholder="Type your blog title"
+        />
+
+        <label>Body:</label>
+        <textarea required placeholder="Write yuor blog">
+        </textarea>
+
+        <label>Author:</label>
+        <input
+          type="text"
+          required
+          placeholder="Your Name"
+        />
+
+        <button>Add Blog</button>
+
+      </form>
     </div>
 
   );
