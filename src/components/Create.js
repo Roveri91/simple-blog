2153f46216626b6ee533e57bc443763a81bfba70
Simone Roveri <spikeroveri91@gmail.com>
2023-11-02 08:41:39 +0900

    form now retrive value

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('');
+
   return (
     <div className="create">
       <h2>Add a new blog</h2>
@@ -8,10 +14,17 @@ const Create = () => {
           type="text"
           required
           placeholder="Type your blog title"
+          value = {title}
+          onChange={(e) => setTitle(e.target.value)}
         />
 
         <label>Body:</label>
-        <textarea required placeholder="Write yuor blog">
+        <textarea
+          required
+          placeholder="Write yuor blog"
+          value = {body}
+          onChange={(e) => setBody(e.target.value)}
+        >
         </textarea>
 
         <label>Author:</label>
@@ -19,9 +32,12 @@ const Create = () => {
           type="text"
           required
           placeholder="Your Name"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
         />
 
         <button>Add Blog</button>
+        <p>{ author }</p>
 
       </form>
     </div>
