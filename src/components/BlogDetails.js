7e284a2769996a9fcf011c3607edc0a61bd7976b
Simone Roveri <spikeroveri91@gmail.com>
2023-11-02 10:13:55 +0900

    Can delete post

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,10 +1,22 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import Loading from "./Loading";
 import useFetch from "../useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
   const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+  const history = useHistory();
+
+  const handleClick = () => {
+    const confirmation = window.confirm('Are you sure you want delete this blog?')
+    if (confirmation){
+      fetch('http://localhost:8000/blogs/' + blog.id, {
+        method: 'DELETE'
+      }).then(() => {
+        history.push('/');
+      })
+    }
+  }
 
   return (
     <div>
@@ -16,6 +28,7 @@ const BlogDetails = () => {
             <h2>{ blog.title }</h2>
             <p><i>Written By:</i> { blog.author }</p>
             <div>{ blog.body }</div>
+            <button onClick={handleClick}>Delete</button>
           </article>
         </div>
       )}
--- src/components/Create.js
+++ src/components/Create.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
+// import { v4 as uuidv4 } from "uuid";
 
 const Create = () => {
   const [title, setTitle] = useState('');
